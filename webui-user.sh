ca0559651a9b7e824823699da070bce59e551307
Gennicks <127051528+Gennicks@users.noreply.github.com>
2023-05-09 21:43:34 +0300

    Update and rename webui-user-new.sh to webui-user.sh

---
+++ webui-user.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+export COMMANDLINE_ARGS="
+--skip-python-version-check 
+--update-check 
+--administrator 
+--xformers 
+--opt-channelslast 
+--listen 
+--port 7887 
+--server-name 0.0.0.0 
+--theme dark 
+--enable-insecure-extension-access 
+--update-all-extensions 
+--allow-code
+--no-progressbar-hiding 
+--gradio-auth 123:123 
+"
